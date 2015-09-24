02c85c0437a274b88a600ca8f3b333a6cc3d3ee3
Ryan Poplawski <claydewd@gmail.com>
2015-09-23 19:06:31 -0500

    updated controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  def index
+      #render text: "params : #{params.inspect}"
+        render template: 'posts/index.html.erb', locals: { posts: Post.all}
+  end
+
+  def new
+
+    render tempate: 'posts/new.html.erb', locals: { post: Post.new }
+  end
+
+  def show
+    #render text: "params : #{params.inspect}"
+    render template: 'posts/show.html.erb', locals: { post: Post.find(params[:id])}
+
+  end
+
+  def create
+    # message, user_id
+    post = Post.new
+    post.body      = params.fetch(:post).fetch(:body)
+    user           = params.fetch(:username)
+    user_id        = User.where(username: user)
+    post.user_id   = User.find(:first)
+
+    post.save
+    redirect_to posts_path
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+      #render text: "params : #{params.inspect}"
+    render template: 'users/index.html.erb', locals: { users: User.all}
+  end
+
+
+  def new
+      #render text: "params : #{params.inspect}"
+    render tempate: 'users/new.html.erb', locals: { user: User.new }
+  end
+
+  def show
+    #render text: "params : #{params.inspect}"
+   render template: 'users/show.html.erb', locals: { user: User.find(params[:id])}
+  end
+
+  def create
+    # message, user_id
+    user = Post.new
+    user.username     = params.fetch(:user).fetch(:username)
+    user.save
+    if user.save
+      redirect_to user_path(User)
+    else
+      redirect_to user_path
+    end
+  end
+end
